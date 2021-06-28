2d6eabcb6e292c0f48216990fce7c7426fd97d4d
mcduck <jjjack1000@gmail.com>
2021-06-28 17:07:59 -0400

    first commit

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    bool canJump = false;
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKey("a")) {
+            body.AddForce(new Vector2(-10,0));
+        }
+        else if (Input.GetKey("d"))
+        {
+            body.AddForce(new Vector2(10, 0));
+        }
+
+        if (Input.GetKey("space")&&canJump) {
+            body.AddForce(new Vector2(0, 200));
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        canJump = false;
+    }
+
+}
---
+++ Assets/gun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class gun : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+
+        transform.position = player.position;
+        
+
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x);
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z * Mathf.Rad2Deg);
+
+        transform.position += new Vector3(Mathf.Cos(rot_z), Mathf.Sin(rot_z), 0)*3;
+    }
+}
