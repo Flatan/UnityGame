264d3cadbb568335b21241032cc8b52632fcc53e
mcduck <jjjack1000@gmail.com>
2021-07-02 04:26:24 -0400

    add gun firing animation

--- Assets/gun.cs
+++ Assets/gun.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+
 
 
 public class gun : MonoBehaviour
 {
+    bool firing = false;
+    Sprite IDLE, FIRING;
     public Transform player;
+
+    Thread fireThread;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fireThread = new Thread(new ThreadStart(Fire));
+        IDLE = Resources.Load<Sprite>("gun");
+        FIRING = Resources.Load<Sprite>("gun_fire");
     }
 
     // Update is called once per frame
@@ -19,7 +27,7 @@ public class gun : MonoBehaviour
 
         transform.position = player.position;
         
-
+        
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diff.Normalize();
 
@@ -30,9 +38,29 @@ public class gun : MonoBehaviour
         transform.position += new Vector3(Mathf.Cos(rot_z), Mathf.Sin(rot_z), 0)*3;
 
         AudioSource audio = GetComponent<AudioSource>();
-        if (Input.GetMouseButton(0) && !audio.isPlaying)
+        if (Input.GetMouseButton(0))
         {
-            audio.Play();
+            if (!fireThread.IsAlive)
+            {
+                fireThread = new Thread(new ThreadStart(Fire));
+                fireThread.Start();
+            }
+            if (!audio.isPlaying)
+                audio.Play();
         }
+
+        if(firing)
+            gameObject.GetComponent<SpriteRenderer>().sprite = FIRING;
+        else
+            gameObject.GetComponent<SpriteRenderer>().sprite = IDLE;
+
+    }
+
+    void Fire() 
+    {
+        firing = true;
+        Thread.Sleep(60);
+        firing = false;
+        Thread.Sleep(60);
     }
 }
