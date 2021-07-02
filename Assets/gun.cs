b6c8b04d17356f56efa341f55812379fdf189e02
mcduck <jjjack1000@gmail.com>
2021-07-01 20:19:29 -0400

    add everything hopefully

--- Assets/gun.cs
+++ Assets/gun.cs
@@ -28,5 +28,11 @@ public class gun : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z * Mathf.Rad2Deg);
 
         transform.position += new Vector3(Mathf.Cos(rot_z), Mathf.Sin(rot_z), 0)*3;
+
+        AudioSource audio = GetComponent<AudioSource>();
+        if (Input.GetMouseButton(0) && !audio.isPlaying)
+        {
+            audio.Play();
+        }
     }
 }
